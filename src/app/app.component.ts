d3bcb6183c765d46861afcea66c54f2c91395ff9
Ardouz11 <rachidardouz11@gmail.com>
2021-03-01 19:59:54 +0100

    Add NavBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template:`
+  <nav-bar><nav-bar>
+  <body><body>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
